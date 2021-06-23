ace43cdb0820ec39ddb285220450aa723f310920
Rafael Andrade <rafaelhandrade@outlook.com>
2021-06-22 21:38:07 -0300

    Third day content

---
+++ src/database/migrations/1624404475338-CreateTags.ts
@@ -0,0 +1,40 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateTags1624404475338 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.createTable(
+            new Table({
+                name : 'tags',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'varchar',
+                        isPrimary: true
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar'
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    },
+                ]
+            })
+        );
+
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('tags');
+    }
+
+}
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
-import express from 'express';
+import 'express-async-errors';
+import express, { NextFunction, Request, Response } from 'express';
 
 import { router } from './routes';
 import './database';
@@ -9,5 +10,21 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction)=>{
+
+    if(error instanceof Error){
+        return response.status(400).json({
+            success : false,
+            error   : error.message
+        });
+    }
+
+    return response.status(500).json({
+        success : false,
+        error   : 'Internal server error'
+    });
+
+})
 
 app.listen(3000, () => console.log('Server is running nlw valoriza'));
